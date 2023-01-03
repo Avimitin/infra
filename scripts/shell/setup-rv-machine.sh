dd0cc2fac33b04265742ea09b4da3be0dccf5a24
Avimitin <avimitin@gmail.com>
2023-01-03 17:26:33 +0800

    Add sha256sums check
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- scripts/shell/setup-rv-machine.sh
+++ scripts/shell/setup-rv-machine.sh
@@ -9,9 +9,21 @@ resources=(
   ["$HOME/.tmux.conf"]="Avimitin/tmux/master/.tmux.conf.min"
 )
 
+declare -A sha256sums
+sha256sums=(
+  ["$HOME/.vimrc"]="08a84017c3c32f61dac8e3bda2bda60774c5e94fdc1c60f2b4add65f8bfa9364"
+  ["$HOME/.bashrc"]="e4e3b54204e04359f88e421d3fe43a962ac28e2becb6bf0cc19c44672c890f8e"
+  ["$HOME/.tmux.conf"]="5f67c6b7221b57e695068f5c412794509ca89864901f808b0c203a6cf4b37b86"
+)
+
 for output in ${!resources[@]}; do
   remote="${prefix}/${resources[$output]}"
   echo "Downloaing $remote to $output"
 
   curl -sSfLo "$output" "$remote"
+
+  printf "Checking file integrity: "
+  sum="${sha256sums[$output]}"
+  echo "$sum  $output" | sha256sum --check
+  echo
 done
